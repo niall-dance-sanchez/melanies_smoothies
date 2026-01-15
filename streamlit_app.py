22a473847c4281ec4708fa34a36ff6efc375ad64
niall-dance-sanchez <niall0205@hotmail.co.uk>
2026-01-15 13:04:27 +0000

    using pandas dataframe for intended searching functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ my_dataframe = session.table(
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients',
@@ -37,6 +37,11 @@ if ingredients_list:
 
     for fruit in ingredients_list:
         ingredients_string += fruit + ' '
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME']
+                              == fruit, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit, ' is ', search_on, '.')
+
         st.subheader(fruit + ' Nutritional Information')
         smoothiefroot_response = requests.get(
             f"https://my.smoothiefroot.com/api/fruit/{fruit}")
